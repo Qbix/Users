c4f2161a482b29d3bd3d76673ecb94972236002f
Greg Magarshak <greg@qbix.com>
2024-11-05 23:39:32 -0500

    Users: Added dummy method implementation to Users_ExernalTo_Ios

--- classes/Users/Device/Ios.php
+++ classes/Users/Device/Ios.php
@@ -113,6 +113,11 @@ class Users_Device_Ios extends Users_Device
 			));
 		}
 	}
+
+	function fetchXids(array $roleIds, array $options = array())
+    {
+        return array();
+    }
 	
 	static protected $push = null;
 }
