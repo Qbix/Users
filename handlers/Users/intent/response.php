503ea1d85c3e2d22084a701ff5481e1dcbf22ab3
Greg Magarshak <greg@qbix.com>
2025-02-16 19:21:14 -0500

    Implemented general purpose intents

--- handlers/Users/intent/response.php
+++ handlers/Users/intent/response.php
@@ -10,11 +10,13 @@
  * @param {string} $_REQUEST.action Required. The desired action, e.g. 'Users/authenticate'
  * @param {string} $_REQUEST.platform Required. The external platform to redirect to afterwards
  * @param {string} [$_REQUEST.appId] Optional. The external platform appId used to load info
- *   with fields to interpolate into the redirection URL
+  *   with fields to interpolate into the redirection URL
+ * @param {string} [$_REQUEST.redirect="redirect"] Optional. Names a diffent config field under
+ *    the config Users/intents/actions/$action/$platform, default is "redirect"
  * @param {array} [$_REQUEST.instructions] Any additional instructions for the action, 
  *   if listed under Users/intents/actions/$action/instructions config
  * @param {array} [$_REQUEST.interpolate] Any additional fields to interpolate into the pattern
- *   found in config under Users/intents/actions/$action/$platform/pattern
+ *   found in config under Users/intents/actions/$action/$platform/$field
  * @return {void}
  */
 function Users_intent_response()
@@ -33,11 +35,12 @@ function Users_intent_response()
         return false;
     }
     $appId = Q::ifset($_REQUEST, 'appId', Q::app());
+    $field = Q::ifset($_REQUEST, 'field', 'redirect');
     list($appId, $info) = Users::appInfo($platform, $appId);
     $interpolate = Q::ifset($_REQUEST, 'interpolate', array());
     $intent = Users_Intent::newIntent($action);
     $params = array_merge($info, $interpolate, array('token' => $intent->token));
-    $pattern = Q_Config::expect('Users', 'intents', 'actions', $action, $platform, 'pattern');
+    $pattern = Q_Config::expect('Users', 'intents', 'actions', $action, $platform, $field);
     $url = Q::interpolate($pattern, $params);
     Q_Response::redirect($url);
     return false; // to prevent XSS, don't return anything to JS, simply redirect the user
