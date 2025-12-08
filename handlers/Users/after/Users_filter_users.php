66356d34786f2e55d327515eba4918cd52abe06b
Greg Magarshak <greg@qbix.com>
2025-12-08 19:59:37 +0400

    Added filter to filter by user id prefix

--- handlers/Users/after/Users_filter_users.php
+++ handlers/Users/after/Users_filter_users.php
@@ -2,6 +2,19 @@
 
 function Users_after_Users_filter_users($params, &$result)
 {
+    // hide by prefixes
+    $hiddenUserIds = array();
+    $prefixes = Q_Config::get('Users', 'filter', 'byIdPrefixes', array());
+    foreach ($prefixes as $p) {
+        foreach ($result as $userId) {
+            if (Q::startsWith($userId, $p)) {
+                $hiddenUserIds[] = $userId;
+            }
+        }
+    }
+    $result = array_values(array_diff($result, $hiddenUserIds));
+
+    // hide by Users/hidden role
     $hiddenRole = 'Users/hidden';
     $communityId = Users::currentCommunityId(true);
     $roles = Users::roles($communityId);
